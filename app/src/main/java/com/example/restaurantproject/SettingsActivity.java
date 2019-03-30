1e18fd11347d035a0f5ca1c54067a846d326601d
Pongpeera Sukasem <pongpeera_sukasem@hotmail.com>
2019-03-30 09:53:55 +0700

    integrated food menu

--- app/src/main/java/com/example/restaurantproject/SettingsActivity.java
+++ app/src/main/java/com/example/restaurantproject/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.restaurantproject;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,44 +10,25 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.Locale;
 
 public class SettingsActivity extends AppCompatActivity {
-    Spinner spinLanguage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        spinLanguage = findViewById(R.id.spinner_language);
-        ArrayAdapter<String> mAdapter = new ArrayAdapter<String>(SettingsActivity.this,
-                android.R.layout.simple_spinner_dropdown_item,
-                getResources().getStringArray(R.array.language_option));
-        spinLanguage.setAdapter(mAdapter);
-
-        String currentLocale = Locale.getDefault().getLanguage();
-        if (currentLocale.equals("th")) spinLanguage.setSelection(mAdapter.getPosition("ไทย"));
-        else spinLanguage.setSelection(mAdapter.getPosition("English"));
-
-        spinLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Context context = SettingsActivity.this;
-                Locale locale;
-                if (i == 1) locale = new Locale("th");
-                else locale = new Locale("en");
-                Locale.setDefault(locale);
-                Configuration configuration = context.getResources().getConfiguration();
-                configuration.setLocale(locale);
-                configuration.setLayoutDirection(locale);
-                context.createConfigurationContext(configuration);
-                System.out.println("it's here");
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SharedPreferences sharedPref = this.getSharedPreferences("selectedLanguage", Context.MODE_PRIVATE);
+        String languageToLoad = sharedPref.getString("language", "");
+        Locale locale = new Locale(languageToLoad);//Set Selected Locale
+        Locale.setDefault(locale);//set new locale as default
+        Configuration config = new Configuration();//get Configuration
+        config.locale = locale;//set config locale as selected locale
+        this.getResources().updateConfiguration(config, this.getResources().getDisplayMetrics());
     }
 }
